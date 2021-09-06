595c25e3be09e78285aaed0d69719a1d595bf671
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-09-06 20:34:14 +0700

    Add project files.

---
+++ SK-Lib-cmath-isunordered-Bagian3__CPP/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*
+	Source by GeeksForGeeks
+	Modified For Learn by RK
+	I.D.E : VS2019
+*/
+
+using namespace std;
+
+int main() {
+	float x = 4.6;
+	float y = 9.2;
+
+	cout << "The value of x is= " << x << endl;
+	cout << "The value of y is= " << y << endl;
+
+	cout << "isunordered(x, y) = " << isunordered(x, y);
+
+	_getch();
+	return 0;
+}
